36794caf1e5a43a0f0fff9afa7e04e2f6f76ccd2
WeAreDevs-ops <cleencuevas@gmail.com>
2025-09-19 20:24:52 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const fetch = globalThis.fetch || (async (...args) => {
 });
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON
 app.use(express.json());
